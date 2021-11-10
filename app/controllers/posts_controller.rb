ce2e62263ef7db7ef8ce8c0eb47be9ba98ebc93f
DAngelo Thomas <dangelo1thomas@gmail.com>
2021-11-10 18:48:27 -0500

    added  basic seed information

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -4,4 +4,14 @@ class PostsController < ApplicationController
         posts = Post.all
         render json: posts
     end
+
+    def create
+        post = Post.create(post_params)
+        render json: post
+    end
+
+    def post_params
+        params.require(:post).permit(:image, :description, :user_id, :likes)
+        # byebug
+    end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+user = User.create(username: "D'Angelo Thomas")
+
+Post.create(image: "Hendrix-electric ladyland", description: "Jimi Hendrix", likes: 0, user: user)
+Post.create(image: "Parachutes", description: "Cold Play", likes: 0, user: user)
+Post.create(image: "", description: "Faker::Music.band", likes: 0, user: user)
+Post.create(image: "Man on The Moon", description: "Kid-Cudi", likes: 0, user: user)
+puts "your seeds are working correctly!!"
